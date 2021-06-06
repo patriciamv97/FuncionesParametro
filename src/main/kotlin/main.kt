2f41cf9ae7eafb2db082ddec9f50933303af2cae
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-06-06 18:17:21 +0200

    Ejercicio2 terminado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,12 @@
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
+    /*
     for (tabla7 in Ejercicio1(10).array){
         println("$tabla7")
     }
+
+     */
+
+    val array = Ejercicio2(10){ Random.nextInt(50,250)}
 }
